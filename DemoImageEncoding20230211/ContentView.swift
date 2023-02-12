bf1c086c2b316f5596a51164665810fc9625d17a
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-11 21:59:28 -0500

    DemoImageEncoding20230211

--- DemoImageEncoding20230211/ContentView.swift
+++ DemoImageEncoding20230211/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    let str = encodedImage("demo")
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Text("Image Encoded")
+                .font(.title2)
+                .bold()
+            Text(str)
+                .lineLimit(10)
+            Spacer()
+            
+            decodedImage(str)!
+                .resizable()
+                .frame(width: 150, height: 150, alignment: .center)
+            Text("Image Decoded")
+                .font(.title2)
+                .bold()
+            Spacer()
         }
         .padding()
     }
@@ -23,4 +35,15 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func encodedImage(_ imageName: String) -> String {
+    UIImage(named: imageName)?.pngData()?.base64EncodedString() ?? ""
+}
+
+func decodedImage(_ encodedImage: String) -> Image? {
+    guard let imageData = Data(base64Encoded: encodedImage) else {
+        return nil
+    }
+    return Image(uiImage: UIImage(data: imageData)!)
 }
