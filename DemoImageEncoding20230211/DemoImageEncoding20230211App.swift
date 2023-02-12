ed099cf474d7e3dd516661425da5db42f06a4db3
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-11 21:03:11 -0500

    Initial Commit

---
+++ DemoImageEncoding20230211/DemoImageEncoding20230211App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoImageEncoding20230211App.swift
+//  DemoImageEncoding20230211
+//
+//  Created by Xavier on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoImageEncoding20230211App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
